4de1d329537033dff64750ba3dd55b1d10665bb5
Viktor Nymark <vnymark@gmail.com>
2019-08-25 18:10:55 +0200

    Added routing and a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+    <b-navbar-nav>
+      <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+      <b-nav-item href="#">X</b-nav-item>
+      <b-nav-item href="#">X</b-nav-item>
+    </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
